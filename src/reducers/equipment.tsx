3363b4fb3ccfb2da1ba513b83a04e70ebeb3b6ee
Wouter van den Heuvel <exo_00@hotmail.com>
2018-11-21 16:52:11 +0100

    Fixed error

--- src/reducers/equipment.tsx
+++ src/reducers/equipment.tsx
@@ -1,7 +1,7 @@
 import { Reducer } from 'redux';
 import { EquipmentStoreState, initialState } from '../stores/equipment';
 import { INCREMENT_RESOURCE } from '../constants/index';
-import { IncrementStructure } from '../actions';
+import { StructureAction } from 'src/actions';
 
 
 /**
@@ -9,15 +9,15 @@ import { IncrementStructure } from '../actions';
  * @param state 
  * @param action 
  */
-export const equipment : Reducer<EquipmentStoreState> = (state:EquipmentStoreState = initialState, action: IncrementStructure) => {
-    switch (action.type) {
-        case INCREMENT_RESOURCE:
-            if(state[action.resource] === undefined) return state;
+export const equipment : Reducer<EquipmentStoreState> = (state:EquipmentStoreState = initialState, action: StructureAction) => {
+    // switch (action.type) {
+    //     case INCREMENT_RESOURCE:
+    //         if(state[action.resource] === undefined) return state;
 
-            return { 
-                ...state,                    
-                [action.resource]: state[action.resource] + 1
-            };                
-    } 
+    //         return { 
+    //             ...state,                    
+    //             [action.resource]: state[action.resource] + 1
+    //         };                
+    // } 
     return state;
 }
