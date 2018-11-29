3690b572133baecd49cb5af65da31f2a4bcad1ca
Wouter van den Heuvel <exo_00@hotmail.com>
2018-11-29 23:52:54 +0100

    resource tick is now refactored

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,11 @@ import TempView from './containers/TempView';
 import { updateTasks } from './actions/tasks';
 import { TaskStoreState } from './stores/task';
 import { TasksStoreState } from './stores/tasks';
+import { StructuresStoreState } from './stores/structures';
+import { StructureDefinition, StructureType, ResourceStructureLevelDefinition, ResourceStructureDefinition } from './definitions/structures';
+import { addResource, addResources } from './actions/resources';
+import structureDefinitions from './definitions/structures';
+import { ResourceStoreState } from './stores/resources';
 
 const store = createStore<StoreState, any, any, any>(
     rootReducer, 
@@ -40,12 +45,42 @@ const processTasks = (tasks:TasksStoreState) => {
     tasks.completed.forEach(task => handleCompletedTask(task));
 }
 
+
+// TODO: place these 'controllers' somewhere else
+
+// Will generate resources based on the structures built
+const processStructures = (structures:StructuresStoreState) => {
+    // Very temporary
+
+    const resourcesToAdd:ResourceStoreState = {};
+    const handleStructure = (structure:string) => {     
+        const structureDefinition:StructureDefinition = structureDefinitions[structure];
+        
+        switch(structureDefinition.type) {
+            case StructureType.resource:
+                const resourceStructureDefinition = structureDefinition as ResourceStructureDefinition;
+                const level:number = structures[structure].level || 0;
+                const levelDefinition:ResourceStructureLevelDefinition = resourceStructureDefinition.levels[level];
+        
+                Object.keys(levelDefinition.generates).reduce((accumulator:ResourceStoreState, resource:string) => {
+                    const amount:number = levelDefinition.generates[resource] * structures[structure].workers;
+                    accumulator[resource] = (accumulator[resource] || 0) + amount;
+                    return accumulator;
+                }, resourcesToAdd);
+            break;
+        }
+    };
+
+    Object.keys(structures).forEach(structure => handleStructure(structure));    
+    store.dispatch(addResources(resourcesToAdd));
+}
+
 store.dispatch(addGold(40)); 
 setInterval(() => {
     var state:StoreState = store.getState();
 
 //    state.
-   // processStructures(state.structures);
+    processStructures(state.structures);
     processTasks(state.tasks);
     //store.dispatch(gameTick())
 
