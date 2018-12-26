955089aef57f8a6fcb69ac72f3a1a915cdedd582
Wouter van den Heuvel <exo_00@hotmail.com>
2018-12-26 19:44:00 +0100

    Refactored structures

--- src/stores/structures.tsx
+++ src/stores/structures.tsx
@@ -1,46 +1,30 @@
-import weaponsmith from "src/definitions/structures/weaponsmith";
+import { Structure } from "src/definitions/structures";
 import { StructureStoreState } from "./structure";
 
 export interface StructuresStoreState {
-    lumberMill: StructureStoreState;
-    ironMine: StructureStoreState;
-    // forge: StructureStoreState,
-    weaponsmith: StructureStoreState;
-    armoursmith: StructureStoreState;
-    // stables: StructureStoreState,
-    tannery: StructureStoreState;
-    garden: StructureStoreState;
-    // alchemist: StructureStoreState,
-    warehouse: StructureStoreState;
+    [Structure.alchemist]: StructureStoreState,
+    [Structure.armoursmith]: StructureStoreState,
+    [Structure.garden]: StructureStoreState,
+    [Structure.lumberMill]: StructureStoreState,
+    [Structure.mine]: StructureStoreState,
+    [Structure.quarry]: StructureStoreState,
+    [Structure.tavern]: StructureStoreState,
+    [Structure.tannery]: StructureStoreState,
+    [Structure.warehouse]: StructureStoreState,
+    [Structure.weaponsmith]: StructureStoreState,
+    [Structure.weaver]: StructureStoreState,
 }
 
 export const initialState: StructuresStoreState = {
-    armoursmith: {
-        level: 0,
-        workers: 1,
-    },
-    garden: {
-        level: 0,
-        workers: 1,
-    },
-    ironMine: {
-        level: 0,
-        workers: 0,
-    },
-    lumberMill: {
-        level: 0,
-        workers: 2,
-    },
-    tannery: {
-        level: 0,
-        workers: 1,
-    },
-    warehouse: {
-        level: 1,
-        workers: 1,
-    },
-    weaponsmith: {
-        level: 0,
-        workers: 1,
-    },
+    [Structure.alchemist]: { level: 0, workers: 1 },
+    [Structure.armoursmith]: { level: 0, workers: 0 },
+    [Structure.garden]: { level: 0, workers: 0 },
+    [Structure.lumberMill]: { level: 0, workers: 0 },
+    [Structure.mine]: { level: 0, workers: 0 },
+    [Structure.quarry]: { level: 0, workers: 0 },
+    [Structure.tavern]: { level: 0, workers: 0 },
+    [Structure.tannery]: { level: 0, workers: 0 },
+    [Structure.warehouse]: { level: 0, workers: 0 },
+    [Structure.weaponsmith]: { level: 0, workers: 0 },
+    [Structure.weaver]: { level: 0, workers: 0 },
 };
