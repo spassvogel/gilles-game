ae9114d4cd1698ccf6a130f7f862ef9ab41030e8
Wouter van den Heuvel <exo_00@hotmail.com>
2019-01-21 17:11:24 +0100

    Increased version to 0.0.3

--- src/constants/version.tsx
+++ src/constants/version.tsx
@@ -2,7 +2,7 @@ import { convertSemVerToInt } from "src/utils/version";
 
 // <MAJOR>.<MINOR>.<RELEASE>
 
-const version = "0.0.2";
+const version = "0.0.3";
 
 export default version;
 export const asInt: number = convertSemVerToInt(version);
