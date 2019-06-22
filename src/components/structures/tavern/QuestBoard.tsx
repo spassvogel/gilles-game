454cfbb133ab406c4a1b9310d19db3faf884c851
wouter <exo_00@hotmail.com>
2019-06-22 14:17:13 +0200

    Split QuestBoard into it's own component

---
+++ src/components/structures/tavern/QuestBoard.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import { AppContextProps } from "src/components/App";
+import { QuestStoreState } from "src/stores/quest";
+import { TextManager } from "src/utils/textManager";
+import "./css/tavernstructureview.css";
+
+// tslint:disable-next-line:no-empty-interface
+export interface DispatchProps {
+//    onUpgrade?: (cost: number) => void;
+//   onCraft?: (productionDefinition: ProductionDefinition, workers: number) => void;
+}
+
+export interface StateProps {
+    availableQuests: QuestStoreState[];
+}
+
+// tslint:disable-next-line:no-empty-interface
+export interface Props {
+/*    type: Structure;*/
+}
+
+type AllProps = Props & StateProps & DispatchProps;
+
+// tslint:disable-next-line:no-empty-interface
+interface LocalState {
+    selectedQuest: string | null;
+}
+
+export default class QuestBoard extends React.Component<AllProps, LocalState> {
+
+    constructor(props: AllProps) {
+        super(props);
+
+        this.state = {
+            selectedQuest: null,
+        };
+    }
+
+    public render() {
+        const questListContent: JSX.Element[] = this.props.availableQuests.map((q) => {
+            const iconImgPath = `img/sigils/${ q.icon }`;
+            const className = "quest" + ((q.name === this.state.selectedQuest) ? " selected" : "");
+            return <li key={ q.name } className = { className } onClick = { () => { this.handleQuestClick(q.name); } }>
+                <div
+                    className = "icon"
+                    style={{backgroundImage: `url(${iconImgPath})`}}
+                ></div>
+                <div className = "title">{ TextManager.getQuestTitle(q.name) } </div>
+            </li>;
+        });
+
+        // quest board, expanded quest info + assign adventurers + launch button
+        return <div className = "quest-area">
+            <h2>Quest board</h2>
+            <ul className = "quest-board">
+                { questListContent }
+            </ul>
+        </div>;
+    };
+   
+
+    private handleQuestClick(name: string) {
+        if (this.state.selectedQuest === name) {
+            this.setState( { selectedQuest: null });
+        } else {
+            this.setState( { selectedQuest: name });
+        }
+    }
+}
--- src/components/structures/tavern/TavernStructureView.tsx
+++ src/components/structures/tavern/TavernStructureView.tsx
@@ -7,6 +7,7 @@ import { AdventurerStoreState } from "src/stores/adventurer";
 import { QuestStoreState } from "src/stores/quest";
 import { TextManager } from "src/utils/textManager";
 import "./css/tavernstructureview.css";
+import QuestBoard from './QuestBoard';
 
 // The UI for the tavern
 // tslint:disable-next-line:no-empty-interface
@@ -87,44 +88,15 @@ export default class TavernStructureView extends React.Component<AllProps, Local
             </div>;
         };
 
-        const createQuestArea = () => {
-            const questListContent: JSX.Element[] = this.props.availableQuests.map((q) => {
-                const iconImgPath = `img/sigils/${ q.icon }`;
-                const className = "quest" + ((q.name === this.state.selectedQuest) ? " selected" : "");
-                return <li key={ q.name } className = { className } onClick = { () => { this.handleQuestClick(q.name); } }>
-                    <div
-                        className = "icon"
-                        style={{backgroundImage: `url(${iconImgPath})`}}
-                    ></div>
-                    <div className = "title">{ TextManager.getQuestTitle(q.name) } </div>
-                </li>;
-            });
-
-            // quest board, expanded quest info + assign adventurers + launch button
-            return <div className = "quest-area">
-                <h2>Quest board</h2>
-                <ul className = "quest-board">
-                    { questListContent }
-                </ul>
-            </div>;
-        };
-
+        
         return (
             <details open = { true } className = "tavernstructureview">
                 <summary>{displayName}</summary>
                 <section>
                     { createRooms() }
-                    { createQuestArea() }
+                    <QuestBoard availableQuests={this.props.availableQuests}/>
                 </section>
             </details>
         );
-    }
-
-    private handleQuestClick(name: string) {
-        if (this.state.selectedQuest === name) {
-            this.setState( { selectedQuest: null });
-        } else {
-            this.setState( { selectedQuest: name });
-        }
     }
 }
