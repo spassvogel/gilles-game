f3048fada85c3e362702bd2178187919238e1ff8
wouter <exo_00@hotmail.com>
2019-07-18 00:49:23 +0200

    Tabs in the log

--- src/components/log/SimpleLog.tsx
+++ src/components/log/SimpleLog.tsx
@@ -1,6 +1,6 @@
 
 import * as React from "react";
-import { LogEntry } from "src/stores/logEntry";
+import { LogEntry, LogChannel } from "src/stores/logEntry";
 import { TextManager } from "src/utils/textManager";
 import "./css/simplelog.css";
 
@@ -10,6 +10,7 @@ export interface Props {
 
 export interface StateProps  {
     logEntries: LogEntry[];
+    questNames: string[];
 }
 
 // tslint:disable-next-line:no-empty-interface
@@ -18,23 +19,105 @@ export interface DispatchProps {
 
 type AllProps = Props & StateProps & DispatchProps;
 
-const getLogEntryRow = (logEntry: LogEntry) => {
-    const text = TextManager.get(logEntry.key, logEntry.context);
-    return <div className="entry" key={ logEntry.time }>
-        { text }
-    </div>;
-};
+enum TabType {
+    all,
+    town,
+    quest,
+}
 
-const SimpleLog = (props: AllProps) => {
+interface TabDefinition {
+    label: string;
+    tabType: TabType;
+    tabId: string;
+    context?: string;
+}
 
-    return <div className="log">
-        <ul className="tabs">
-            <li>all</li>
-            <li>town</li>
-        </ul>
-        <div className="log-entries">
-            { props.logEntries.map((entry) => getLogEntryRow(entry))}
-        </div>
-    </div>;
-};
+interface LocalState {
+    selectedTabId: string;
+}
+
+class SimpleLog extends React.Component<AllProps, LocalState> {
+
+    constructor(props: AllProps) {
+        super(props);
+
+        this.state = {
+            selectedTabId: "all",
+        };
+    }
+
+    public render() {
+
+        const tabs: TabDefinition[] = [{
+            label: TextManager.get("common-log-tab-all"),
+            tabId: "all",
+            tabType: TabType.all,
+        }, {
+            label: TextManager.get("common-log-tab-town"),
+            tabId: "town",
+            tabType: TabType.town,
+        }];
+
+        this.props.questNames.forEach((questName) => {
+            tabs.push({
+                context: questName,
+                label: TextManager.getQuestTitle(questName),
+                tabId: `quest-${questName}`,
+                tabType: TabType.quest,
+            });
+        });
+
+        let logEntries: LogEntry[] = [];
+        const currentTab = tabs.find((t) => t.tabId === this.state.selectedTabId)!;
+        switch (currentTab.tabType) {
+            case TabType.all:
+                // All the things
+                logEntries = this.props.logEntries;
+                break;
+
+            case TabType.town:
+                // Only town
+                logEntries = this.props.logEntries.filter((lE) => lE.channel === LogChannel.town);
+                break;
+
+            case TabType.quest:
+                // Only the selected quest
+                logEntries = this.props.logEntries.filter((lE) => lE.channel === LogChannel.quest && lE.context === currentTab.context);
+                break;
+        }
+
+        const getLogEntryRow = (logEntry: LogEntry) => {
+            const text = TextManager.get(logEntry.key, logEntry.context);
+            return <div className = "entry" key={ logEntry.time }>
+                { text }
+            </div>;
+        };
+
+        return <div className = "log">
+            <ul className = "tabs">
+                {
+                    tabs.map((tab) => {
+                        return <li
+                            key = { tab.tabId }
+                            className = { this.state.selectedTabId === tab.tabId ? "active" : "" }
+                            onClick = { () => this.handleTabClick(tab.tabId) }
+                        >
+                            { tab.label }
+                        </li>;
+                    })
+                }
+            </ul>
+            <div className = "log-entries">
+                { logEntries.map((entry) => getLogEntryRow(entry))}
+            </div>
+        </div>;
+    }
+
+    private handleTabClick(tabId: string) {
+        this.setState({
+            selectedTabId: tabId,
+        });
+    }
+}
+
 export default SimpleLog;
--- src/containers/log/SimpleLog.tsx
+++ src/containers/log/SimpleLog.tsx
@@ -2,10 +2,12 @@ import { connect } from "react-redux";
 import { AnyAction, Dispatch } from "redux";
 import SimpleLog, { DispatchProps, Props, StateProps } from "src/components/log/SimpleLog";
 import { StoreState } from "src/stores";
+import { QuestStatus } from "src/stores/quest";
 
 const mapStateToProps = (store: StoreState): StateProps => {
     return {
         logEntries: store.log,
+        questNames: store.quests.filter((q) => q.status === QuestStatus.active).map((q) => q.name),
     };
 };
 
