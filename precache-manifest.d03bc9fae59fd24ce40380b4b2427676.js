5c43ad46a99f4b62628ef65adf3a1a2dd155e08e
spassvogel <exo_00@hotmail.com>
2019-07-18 12:19:20 +0200

    Updates

---
+++ precache-manifest.d03bc9fae59fd24ce40380b4b2427676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72ad28560f55d110c31d9c266882f65",
+    "url": "/gilles-game/index.html"
+  },
+  {
+    "revision": "3777710168872f06262a",
+    "url": "/gilles-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gilles-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3777710168872f06262a",
+    "url": "/gilles-game/static/js/main.fc103b17.chunk.js"
+  },
+  {
+    "revision": "a578b82798e350d0ae8b",
+    "url": "/gilles-game/static/js/runtime~main.89b35411.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gilles-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
