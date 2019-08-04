e236e686caef4c7500c91c2d1ab3d7a6504c201a
wouter <exo_00@hotmail.com>
2019-08-04 11:42:26 +0200

    Refactored TavernStructureView to a pure function

--- src/components/structures/tavern/TavernStructureView.tsx
+++ src/components/structures/tavern/TavernStructureView.tsx
@@ -35,161 +35,305 @@ interface LocalState {
 
 const SOURCE_ID = "tavern";
 
-export default class TavernStructureView extends React.Component<AllProps, LocalState> {
+const TavernStructureView = (props: AllProps) => {
+    const [assignedAventurers, setAassignedAdventurers] = React.useState<AdventurerStoreState[]>([]);
+    const [selectedQuest, setSelectedQuest] = React.useState<string | null>(null);
 
-    constructor(props: AllProps) {
-        super(props);
+    const structureDefinition = getDefinition<TavernStructureDefinition>(Structure.tavern);
+    const level: number = props.level || 0;
+    const levelDefinition: TavernStructureLevelDefinition = structureDefinition.levels[level];
+    const displayName = TextManager.getStructureName(Structure.tavern);
 
-        this.state = {
-            assignedAventurers: [],
-            selectedQuestName: null,
+    const createUpgradeRow = () => {
+        const gold = props.gold;
+        const nextLevel = structureDefinition.levels[level + 1];
+        const nextLevelCost = (nextLevel != null ? nextLevel.cost.gold || 0 : -1);
+        const canUpgrade = nextLevel != null && gold >= nextLevelCost;
+        const upgradeText = `Upgrade! (${nextLevelCost < 0 ? "max" : nextLevelCost + " gold"})`;
+
+        const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+            if (props.onUpgrade) { props.onUpgrade(nextLevelCost, level + 1); }
         };
-    }
+        return <div>
+            <label>level:</label>{ (level + 1) + " / " + structureDefinition.levels.length }
+            <button
+                style = {{ float: "right" }}
+                onClick = { handleClick }
+                disabled= { !canUpgrade }
+            >
+                { upgradeText }
+            </button>
+        </div>;
+    };
 
-    public render() {
-        const structureDefinition = getDefinition<TavernStructureDefinition>(Structure.tavern);
-        const level: number = this.props.level || 0;
-        const levelDefinition: TavernStructureLevelDefinition = structureDefinition.levels[level];
-        const displayName = TextManager.getStructureName(Structure.tavern);
+    const roomCount = levelDefinition.rooms;
+    const createRooms = () => {
 
-        const createUpgradeRow = () => {
-            const gold = this.props.gold;
-            const nextLevel = structureDefinition.levels[level + 1];
-            const nextLevelCost = (nextLevel != null ? nextLevel.cost.gold || 0 : -1);
-            const canUpgrade = nextLevel != null && gold >= nextLevelCost;
-            const upgradeText = `Upgrade! (${nextLevelCost < 0 ? "max" : nextLevelCost + " gold"})`;
+        const roomContent: JSX.Element[] = [];
+        for (let i = 0; i < roomCount; i++) {
+            const adventurer = props.adventurers.find((a) => a.room === i);
+            let content = null;
+            if (adventurer) {
+                let name = adventurer.name;
 
-            const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-                if (this.props.onUpgrade) { this.props.onUpgrade(nextLevelCost, level + 1); }
-            };
-            return <div>
-                <label>level:</label>{ (level + 1) + " / " + structureDefinition.levels.length }
-                <button
-                    style = {{ float: "right" }}
-                    onClick = { handleClick }
-                    disabled= { !canUpgrade }
-                >
-                    { upgradeText }
-                </button>
-            </div>;
-        };
-
-        const roomCount = levelDefinition.rooms;
-        const createRooms = () => {
-
-            const roomContent: JSX.Element[] = [];
-            for (let i = 0; i < roomCount; i++) {
-                const adventurer = this.props.adventurers.find((a) => a.room === i);
-                let content = null;
-                if (adventurer) {
-                    let name = adventurer.name;
-
-                    const assigned = this.state.assignedAventurers.indexOf(adventurer) > -1; // assigned to a quest in the QuestBoard
-                    const party = this.getQuestByAdventurer(adventurer.id);
-                    if (party) {
-                        name += " (on a quest)";
-                    }
-                    content = [<DraggableAdventurerAvatar
-                        disabled = { assigned || party != null }
-                        adventurer = { adventurer }
-                        className = "adventurer-icon"
-                        sourceId = { SOURCE_ID }
-                        key = { `avatar:${adventurer.id}` }
-                    />,
-                    name,
-                    /*
-                    <button
-                        className = "boot"
-                        key = { `boot:${adventurer.id}` }
-                    >
-                        Boot
-                    </button>,*/
-                ];
-                } else {
-                    content = "(empty room)";
+                const assigned = assignedAventurers.indexOf(adventurer) > -1; // assigned to a quest in the QuestBoard
+                const party = getQuestByAdventurer(adventurer.id);
+                if (party) {
+                    name += " (on a quest)";
                 }
-
-                roomContent.push(<div key = { `room${i}` } className = "room">
-                    { content }
-                </div>);
+                content = [<DraggableAdventurerAvatar
+                    disabled = { assigned || party != null }
+                    adventurer = { adventurer }
+                    className = "adventurer-icon"
+                    sourceId = { SOURCE_ID }
+                    key = { `avatar:${adventurer.id}` }
+                />,
+                <a key = { adventurer.id } onClick = { () => handleAdventurerNameClick(adventurer) }> { name }</a>,
+                /*
+                <button
+                    className = "boot"
+                    key = { `boot:${adventurer.id}` }
+                >
+                    Boot
+                </button>,*/
+            ];
+            } else {
+                content = "(empty room)";
             }
 
-            return <div className = "rooms">
-                <h2>Rooms</h2>
-                { roomContent }
-            </div>;
-        };
+            roomContent.push(<div key = { `room${i}` } className = "room">
+                { content }
+            </div>);
+        }
 
-        const availableQuests = this.props.quests.filter((q) => q.status === QuestStatus.available );
+        return <div className = "rooms">
+            <h2>Rooms</h2>
+            { roomContent }
+        </div>;
+    };
 
-        return (
-            <details open = { true } className = "tavernstructureview">
-                <summary>{ displayName }</summary>
-                { createUpgradeRow() }
-                <section>
-                    { createRooms() }
-                    <QuestBoard
-                        availableQuests = { availableQuests }
-                        selectedQuestName = { this.state.selectedQuestName }
-                        assignedAventurers = { this.state.assignedAventurers }
-                        onQuestClick = { (name: string) => this.handleQuestClick(name) }
-                        onAddAdventurer = { (item: AdventurerAvatarDragInfo, index: number) => this.handleAddAdventurer(item, index) }
-                        onRemoveAdventurer = { (index: number) => this.handleRemoveAdventurer(index) }
-                        onLaunchQuest = { () => this.handleLaunchQuest() }
-                    />
-                </section>
-            </details>
-        );
-    }
+    const getAvailableQuests = props.quests.filter((q) => q.status === QuestStatus.available );
 
-    private handleQuestClick(name: string) {
-        if (this.state.selectedQuestName === name) {
-            this.setState( { selectedQuestName: null });
+    const getQuestByAdventurer = (adventurerId: string): QuestStoreState | undefined => {
+        return Object.values(props.quests).find((quest) => {
+            return quest.party.indexOf(adventurerId) > -1;
+        });
+    };
+
+    const handleQuestClick = (name: string) => {
+        if (selectedQuest === name) {
+            setSelectedQuest(null);
         } else {
-            this.setState( { selectedQuestName: name });
+            setSelectedQuest(name);
         }
 
         // Unassign all adventurers
-        this.setState({
-            assignedAventurers: [],
-        });
-    }
+        setAassignedAdventurers([]);
+    };
 
-    private handleAddAdventurer(item: AdventurerAvatarDragInfo, index: number) {
+    const handleAddAdventurer = (item: AdventurerAvatarDragInfo, index: number) => {
         if (item.sourceId === SOURCE_ID) {
-            const assignedAventurers = [ ...this.state.assignedAventurers];
-            assignedAventurers[index] = item.adventurer;
-            this.setState({
-                assignedAventurers,
-            });
+            const copy = [ ...assignedAventurers];
+            copy[index] = item.adventurer;
+            setAassignedAdventurers(copy);
         }
-    }
+    };
 
-    /**
-     * Removes an assigned adventurer from a slot
-     * @param index
-     */
-    private handleRemoveAdventurer(index: number): void {
-        const assignedAventurers = [ ...this.state.assignedAventurers];
-        delete assignedAventurers[index];
-        this.setState({
-            assignedAventurers,
-        });
-    }
+    const handleRemoveAdventurer = (index: number): void => {
+        const copy = [ ...assignedAventurers];
+        delete copy[index];
+        setAassignedAdventurers(copy);
+    };
 
-    private handleLaunchQuest(): void {
-        const assignedAventurers = this.state.assignedAventurers;
-        const selectedQuestName = this.state.selectedQuestName!;
-        this.props.onLaunchQuest(selectedQuestName, assignedAventurers);
-    }
+    const handleLaunchQuest = (): void => {
+        props.onLaunchQuest(selectedQuest!, assignedAventurers);
+    };
 
-    /**
-     * Returns the party the adventurer is in. undefined if not in any party
-     * @param adventurerId
-     */
-    private getQuestByAdventurer(adventurerId: string): QuestStoreState | undefined {
-        return Object.values(this.props.quests).find((quest) => {
-            return quest.party.indexOf(adventurerId) > -1;
-        });
-    }
-}
+    const handleAdventurerNameClick = (adventurer: AdventurerStoreState) => {
+        console.log(adventurer.name)
+    };
+
+    return (
+        <details open = { true } className = "tavernstructureview">
+            <summary>{ displayName }</summary>
+            { createUpgradeRow() }
+            <section>
+                { createRooms() }
+                <QuestBoard
+                    availableQuests = { getAvailableQuests }
+                    selectedQuestName = { selectedQuest }
+                    assignedAventurers = { assignedAventurers }
+                    onQuestClick = { (name: string) => handleQuestClick(name) }
+                    onAddAdventurer = { (item: AdventurerAvatarDragInfo, index: number) => handleAddAdventurer(item, index) }
+                    onRemoveAdventurer = { (index: number) => handleRemoveAdventurer(index) }
+                    onLaunchQuest = { () => handleLaunchQuest() }
+                />
+            </section>
+        </details>
+    );
+};
+
+export default TavernStructureView;
+
+// export default class TavernStructureView extends React.Component<AllProps, LocalState> {
+
+//     constructor(props: AllProps) {
+//         super(props);
+
+//         this.state = {
+//             assignedAventurers: [],
+//             selectedQuestName: null,
+//         };
+//     }
+
+//     public render() {
+//         const structureDefinition = getDefinition<TavernStructureDefinition>(Structure.tavern);
+//         const level: number = this.props.level || 0;
+//         const levelDefinition: TavernStructureLevelDefinition = structureDefinition.levels[level];
+//         const displayName = TextManager.getStructureName(Structure.tavern);
+
+//         const createUpgradeRow = () => {
+//             const gold = this.props.gold;
+//             const nextLevel = structureDefinition.levels[level + 1];
+//             const nextLevelCost = (nextLevel != null ? nextLevel.cost.gold || 0 : -1);
+//             const canUpgrade = nextLevel != null && gold >= nextLevelCost;
+//             const upgradeText = `Upgrade! (${nextLevelCost < 0 ? "max" : nextLevelCost + " gold"})`;
+
+//             const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+//                 if (this.props.onUpgrade) { this.props.onUpgrade(nextLevelCost, level + 1); }
+//             };
+//             return <div>
+//                 <label>level:</label>{ (level + 1) + " / " + structureDefinition.levels.length }
+//                 <button
+//                     style = {{ float: "right" }}
+//                     onClick = { handleClick }
+//                     disabled= { !canUpgrade }
+//                 >
+//                     { upgradeText }
+//                 </button>
+//             </div>;
+//         };
+
+//         const roomCount = levelDefinition.rooms;
+//         const createRooms = () => {
+
+//             const roomContent: JSX.Element[] = [];
+//             for (let i = 0; i < roomCount; i++) {
+//                 const adventurer = this.props.adventurers.find((a) => a.room === i);
+//                 let content = null;
+//                 if (adventurer) {
+//                     let name = adventurer.name;
+
+//                     const assigned = this.state.assignedAventurers.indexOf(adventurer) > -1; // assigned to a quest in the QuestBoard
+//                     const party = this.getQuestByAdventurer(adventurer.id);
+//                     if (party) {
+//                         name += " (on a quest)";
+//                     }
+//                     content = [<DraggableAdventurerAvatar
+//                         disabled = { assigned || party != null }
+//                         adventurer = { adventurer }
+//                         className = "adventurer-icon"
+//                         sourceId = { SOURCE_ID }
+//                         key = { `avatar:${adventurer.id}` }
+//                     />,
+//                     <a key = { adventurer.id } onClick = { () => this.handleAdventurerNameClick(adventurer) }> { name }</a>,
+//                     /*
+//                     <button
+//                         className = "boot"
+//                         key = { `boot:${adventurer.id}` }
+//                     >
+//                         Boot
+//                     </button>,*/
+//                 ];
+//                 } else {
+//                     content = "(empty room)";
+//                 }
+
+//                 roomContent.push(<div key = { `room${i}` } className = "room">
+//                     { content }
+//                 </div>);
+//             }
+
+//             return <div className = "rooms">
+//                 <h2>Rooms</h2>
+//                 { roomContent }
+//             </div>;
+//         };
+
+//         const availableQuests = this.props.quests.filter((q) => q.status === QuestStatus.available );
+
+//         return (
+//             <details open = { true } className = "tavernstructureview">
+//                 <summary>{ displayName }</summary>
+//                 { createUpgradeRow() }
+//                 <section>
+//                     { createRooms() }
+//                     <QuestBoard
+//                         availableQuests = { availableQuests }
+//                         selectedQuestName = { this.state.selectedQuestName }
+//                         assignedAventurers = { this.state.assignedAventurers }
+//                         onQuestClick = { (name: string) => this.handleQuestClick(name) }
+//                         onAddAdventurer = { (item: AdventurerAvatarDragInfo, index: number) => this.handleAddAdventurer(item, index) }
+//                         onRemoveAdventurer = { (index: number) => this.handleRemoveAdventurer(index) }
+//                         onLaunchQuest = { () => this.handleLaunchQuest() }
+//                     />
+//                 </section>
+//             </details>
+//         );
+//     }
+
+//     private handleQuestClick(name: string) {
+//         if (this.state.selectedQuestName === name) {
+//             this.setState( { selectedQuestName: null });
+//         } else {
+//             this.setState( { selectedQuestName: name });
+//         }
+
+//         // Unassign all adventurers
+//         this.setState({
+//             assignedAventurers: [],
+//         });
+//     }
+
+//     private handleAdventurerNameClick(adventurer: AdventurerStoreState) {
+//         //console.log(name)
+//     }
+
+//     private handleAddAdventurer(item: AdventurerAvatarDragInfo, index: number) {
+//         if (item.sourceId === SOURCE_ID) {
+//             const assignedAventurers = [ ...this.state.assignedAventurers];
+//             assignedAventurers[index] = item.adventurer;
+//             this.setState({
+//                 assignedAventurers,
+//             });
+//         }
+//     }
+
+//     /**
+//      * Removes an assigned adventurer from a slot
+//      * @param index
+//      */
+//     private handleRemoveAdventurer(index: number): void {
+//         const assignedAventurers = [ ...this.state.assignedAventurers];
+//         delete assignedAventurers[index];
+//         this.setState({
+//             assignedAventurers,
+//         });
+//     }
+
+//     private handleLaunchQuest(): void {
+//         const assignedAventurers = this.state.assignedAventurers;
+//         const selectedQuestName = this.state.selectedQuestName!;
+//         this.props.onLaunchQuest(selectedQuestName, assignedAventurers);
+//     }
+
+//     /**
+//      * Returns the party the adventurer is in. undefined if not in any party
+//      * @param adventurerId
+//      */
+//     private getQuestByAdventurer(adventurerId: string): QuestStoreState | undefined {
+//         return Object.values(this.props.quests).find((quest) => {
+//             return quest.party.indexOf(adventurerId) > -1;
+//         });
+//     }
+// }
