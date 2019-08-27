162a4815bde1d73ec19e58dc81806ca77ad98f8a
Wouter van den Heuvel <exo_00@hotmail.com>
2019-08-28 00:58:16 +0200

    Fixed some bugs with the testing of threejs model

---
+++ src/Model.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three'
+import React, { useMemo, useState } from 'react'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+
+function Model(props/*: ModelProps | any*/) {
+    const { url } = props;
+    const [geometry, setGeometry] = useState();
+
+    useMemo(() => {
+      console.log("loading")
+      new OBJLoader().load(url, (grp/*: Group*/) => {
+        setGeometry((grp.children[0]/* as Mesh*/).geometry);
+      });
+    }, [url]);
+
+    const cube = new THREE.Mesh( new THREE.CircleBufferGeometry( 200, 200, 200 ), new THREE.MeshNormalMaterial() );
+    cube.position.y = 150;
+
+    //return <primitive object={cube} position={[0, 0, 0]}></primitive>;
+    return geometry ? <mesh 
+      //onClick
+      geometry={ geometry } {...props}
+    >
+      {/* <primitive object={cube} position={[0, 0, 0]}></primitive> */}
+    <meshPhysicalMaterial
+      attach="material"
+      roughness={0.8}
+      metalness={0.6}
+      //emissive="#a4f20d"
+      //emissiveIntensity={active ? 0.1 : 0}
+      color="#1c1c1c"
+      fog={true}
+      //shininess={0.5}
+    />
+  </mesh> : null;
+}
+
+export default Model;
--- src/Thing.tsx
+++ src/Thing.tsx
@@ -4,16 +4,17 @@ import ReactDOM from 'react-dom'
 import { Canvas } from 'react-three-fiber'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-import { Group, Mesh } from "three";
-import { ReactThreeFiber } from "react-three-fiber/types/three";
+import { Group, Mesh, Vector3 } from "three";
+import Model from 'Model';
 
 interface Props {
 }
 
 function Thing(props: Props) {
   return (
-    <Canvas style = {{ height: 800, width: 600 }} camera = {{
-      position: [61.534, 161.089, 658.156]
+    <Canvas style = {{ height: 800, width: 600 }} 
+    camera = {{
+      position: new Vector3(61.534, 161.089, 658.156),
     }}>
      <Model url="models/town/house2/Medieval_House.obj"  position = { [ 1, 1, 1]} />
      <Model url="models/town/house2/Medieval_House.obj"  position = { [ 258.861, 0, -55.582]} />
@@ -24,37 +25,7 @@ function Thing(props: Props) {
 interface ModelProps {
   url: string;
 }
-function Model(props: ModelProps | any) {
-    const { url } = props;
-    const [geometry, setGeometry] = useState();
 
-    useMemo(() => {
-      console.log("loading")
-      new OBJLoader().load(url, (grp: Group) => {
-        setGeometry((grp.children[0] as Mesh).geometry);
-      });
-    }, [url]);
-
-    const cube = new THREE.Mesh( new THREE.CircleBufferGeometry( 200, 200, 200 ), new THREE.MeshNormalMaterial() );
-    cube.position.y = 150;
-
-    return geometry ? <mesh 
-      //onClick
-      geometry={ geometry } {...props}
-    >
-      <primitive object={cube} position={[0, 0, 0]}></primitive>
-    <meshPhysicalMaterial
-      attach="material"
-      roughness={0.8}
-      metalness={0.6}
-      //emissive="#a4f20d"
-      //emissiveIntensity={active ? 0.1 : 0}
-      color="#1c1c1c"
-      fog={true}
-      //shininess={0.5}
-    />
-  </mesh> : null;
-}
 export default Thing;
 
 
