ebc8f9a72ba4518dff0772583b69fc8d2eb7b753
wouter <exo_00@hotmail.com>
2019-10-27 20:06:13 +0100

    fix: Sphere is tsx now

---
+++ src/Sphere.tsx
@@ -0,0 +1,34 @@
+import React, {  } from "react";
+import * as THREE from "three";
+
+export interface Props {
+  name?: string;
+  position?: THREE.Vector3|number[];
+  onClick?: (object: THREE.Object3D) => void;
+}
+
+const Sphere = (props: Props) => {
+    const geometry = new THREE.SphereGeometry( 5, 32, 32 );
+    const material = new THREE.MeshBasicMaterial( {color: 0xfcba03} );
+
+    const handleClick = (evt: any) => {
+      if (props.onClick) {
+        props.onClick(evt.object);
+      }
+    };
+
+    if (!geometry) {
+      return null;
+    }
+    return (
+      <mesh
+        name={ props.name}
+        position={props.position}
+        onClick={ handleClick }
+        geometry={ geometry }
+        material={ material }
+      />
+    );
+  };
+
+export default Sphere;
--- src/components/three/WorldMap.tsx
+++ src/components/three/WorldMap.tsx
@@ -6,12 +6,18 @@ import WorldMapTerrain from "WorldMapTerrain";
 import Sphere from "Sphere";
 import Controls from "Controls";
 import DebugInspector from "DebugInspector";
+import { Object3D, Vector3 } from "three";
 
 interface Props {
 }
 
 function WorldMap(props: Props) {
   console.log('render')
+
+  const handleClick = (object: Object3D) => {
+    console.log(object)
+  };
+
   return (
     <Canvas style = {{ height: 860, width: 648 }} >
       <DebugInspector />
@@ -20,7 +26,8 @@ function WorldMap(props: Props) {
      <Model url="models/town/house2/Medieval_House.obj"  position = { [ 258.861, 0, -55.582]} />
      <Terrain heightmap = { heightmap } position = { [ 258.861, 0, -55.582]} name = "Terrain" /> */}
      <WorldMapTerrain name="terrain"/>
-     <Sphere />
+     <Sphere onClick={handleClick} position={new Vector3(62, 0, 14)} name="party1" />
+     <Sphere onClick={handleClick} name="party2" />
      </Canvas>
   )
 }
