eae6260f118baef06c7b745ffecdf1b5027c7275
Wouter van den Heuvel <exo_00@hotmail.com>
2019-12-05 22:53:14 +0100

    chore: purecomponent for questboard

--- src/components/structures/tavern/QuestBoard.tsx
+++ src/components/structures/tavern/QuestBoard.tsx
@@ -32,84 +32,73 @@ export interface Props {
 
 type AllProps = Props & DispatchProps & StateProps;
 
-// tslint:disable-next-line:no-empty-interface
-interface LocalState {
-}
+const QuestBoard = (props: AllProps) => {
 
-export default class QuestBoard extends React.Component<AllProps, LocalState> {
+    const questListContent: JSX.Element[] = props.availableQuests.map((q) => {
+        const iconImgPath = `img/sigils/${ q.icon }`;
+        const className = "quest" + ((q.name === props.selectedQuestName) ? " selected" : "");
+        return <li key={ q.name } className = { className } onClick = { () => { props.onQuestClick(q.name); } }>
+            <div
+                className = "icon"
+                style={{backgroundImage: `url(${iconImgPath})`}}
+            ></div>
+            <div className = "title">{ TextManager.getQuestTitle(q.name) } </div>
+        </li>;
+    });
 
-    constructor(props: AllProps) {
-        super(props);
+    const getQuestDetails = () => {
+        if (!props.selectedQuestName) {
+            return null;
+        }
+        const quest = props.availableQuests.find((q) => q.name === props.selectedQuestName);
+        if (!quest) {
+            return <div> { TextManager.get("structure-tavern-quest-launched") } </div>;
+        }
+        const questDefinition = getDefinition(quest.name);
 
-        this.state = {
-            selectedQuest: null,
-        };
-    }
+        // Need a full party to launch
+        const fullParty = props.assignedAventurers.filter((a) => a !== null).length >= minimumCountAdventurers;
+        // Check if we have the required items
+        const enoughItems = checkEnoughItems(questDefinition);
 
-    public render() {
-        const questListContent: JSX.Element[] = this.props.availableQuests.map((q) => {
-            const iconImgPath = `img/sigils/${ q.icon }`;
-            const className = "quest" + ((q.name === this.props.selectedQuestName) ? " selected" : "");
-            return <li key={ q.name } className = { className } onClick = { () => { this.props.onQuestClick(q.name); } }>
-                <div
-                    className = "icon"
-                    style={{backgroundImage: `url(${iconImgPath})`}}
-                ></div>
-                <div className = "title">{ TextManager.getQuestTitle(q.name) } </div>
-            </li>;
-        });
+        const canLaunch = fullParty && enoughItems;
 
-        const getQuestDetails = () => {
-            if (!this.props.selectedQuestName) {
-                return null;
-            }
-            const quest = this.props.availableQuests.find((q) => q.name === this.props.selectedQuestName);
-            if (!quest) {
-                return <div> { TextManager.get("structure-tavern-quest-launched") } </div>;
-            }
-            const questDefinition = getDefinition(quest.name);
-
-            // Need a full party to launch
-            const fullParty = this.props.assignedAventurers.filter((a) => a !== null).length >= minimumCountAdventurers;
-            // Check if we have the required items
-            const enoughItems = this.checkEnoughItems(questDefinition);
-
-            const canLaunch = fullParty && enoughItems;
-
-            return <div className="quest-details">
-                { TextManager.getQuestDescription(this.props.selectedQuestName) }
-                <AssignAdventurers
-                    availableSlots = { availableSlots }
-                    assignedAventurers = { this.props.assignedAventurers }
-                    onRemoveAdventurer = { this.props.onRemoveAdventurer }
-                    onAddEventurer = { this.props.onAddAdventurer } />
-                <ItemsCostBox items = { questDefinition.requiredItems || [] }/>
-                <button disabled = { !canLaunch } onClick = { () => this.props.onLaunchQuest() }>
-                    { TextManager.get("structure-tavern-button-launch-quest") }
-                </button>
-            </div>;
-        };
-
-        // quest board, expanded quest info + assign adventurers + launch button
-        return <div className = "quest-board">
-            <h2> { TextManager.get("structure-tavern-title-quest-board") } </h2>
-            <ul className = "quest-list">
-                { questListContent }
-            </ul>
-            { getQuestDetails() }
+        return <div className="quest-details">
+            { TextManager.getQuestDescription(props.selectedQuestName) }
+            <AssignAdventurers
+                availableSlots = { availableSlots }
+                assignedAventurers = { props.assignedAventurers }
+                onRemoveAdventurer = { props.onRemoveAdventurer }
+                onAddEventurer = { props.onAddAdventurer } />
+            <ItemsCostBox items = { questDefinition.requiredItems || [] }/>
+            <button disabled = { !canLaunch } onClick = { () => props.onLaunchQuest() }>
+                { TextManager.get("structure-tavern-button-launch-quest") }
+            </button>
         </div>;
-    }
+    };
 
     /**
-     * Returns true if all the items required by the quest are in the inventory
-     */
-    public checkEnoughItems(questDefinition: QuestDefinition): boolean {
+     * Returns true if all the items required by the quest are in the inventory */
+    const checkEnoughItems = (questDefinition: QuestDefinition): boolean => {
         if (!questDefinition.requiredItems) {
             return true;
         }
         return questDefinition.requiredItems.every((item: Item) => {
             const amountRequired = (questDefinition.requiredItems)!.filter((i) => i === item).length;
-            return this.props.items.filter((i) => i === item).length >= amountRequired;
+            return props.items.filter((i) => i === item).length >= amountRequired;
         });
     }
+
+    // quest board, expanded quest info + assign adventurers + launch button
+    return (
+        <div className = "quest-board">
+            <h2> { TextManager.get("structure-tavern-title-quest-board") }</h2>
+            <ul className = "quest-list">
+                { questListContent }
+            </ul>
+            { getQuestDetails() }
+        </div>
+    );
 }
+
+export default QuestBoard;
