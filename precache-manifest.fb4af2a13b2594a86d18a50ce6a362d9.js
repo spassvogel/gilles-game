8f53b022317d8cf7a8d42aa014eae42e2d5eddf4
Wouter van den Heuvel <exo_00@hotmail.com>
2020-04-14 17:19:20 +0200

    Updates

---
+++ precache-manifest.fb4af2a13b2594a86d18a50ce6a362d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389b16508172392c212e927500c769f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72dbc5fa950fe1c864ce",
+    "url": "./static/css/main.72cf33f2.chunk.css"
+  },
+  {
+    "revision": "fe166851be0420af49cc",
+    "url": "./static/js/2.3838823d.chunk.js"
+  },
+  {
+    "revision": "3afb171569fc3dc942e3941998ddbe0b",
+    "url": "./static/js/2.3838823d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72dbc5fa950fe1c864ce",
+    "url": "./static/js/main.42d17872.chunk.js"
+  },
+  {
+    "revision": "6f16a1d65fb19f219110",
+    "url": "./static/js/runtime-main.b58f9181.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./gilles-game/precache-manifest.05f248520b5f92e3cb19d115af659da7.js"
+  "./precache-manifest.fb4af2a13b2594a86d18a50ce6a362d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./gilles-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
