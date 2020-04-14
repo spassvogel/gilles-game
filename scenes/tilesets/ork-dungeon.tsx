ad635f13e9c51182f827a8e6fa2505d2b566744a
Wouter van den Heuvel <exo_00@hotmail.com>
2020-04-14 16:38:22 +0200

    Updates

---
+++ scenes/tilesets/ork-dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="ork-dungeon" tilewidth="100" tileheight="100" tilecount="100" columns="10">
+ <image source="dungeon.jpg" width="1000" height="1000"/>
+</tileset>
