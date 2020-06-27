e9ed1a608d89a979b0e59d0276e319a9866fcb81
Wouter van den Heuvel <exo_00@hotmail.com>
2020-06-27 12:25:47 +0200

    fix: iron added to resource views

--- src/constants/resources.ts
+++ src/constants/resources.ts
@@ -1,8 +1,10 @@
 import { Resource } from "definitions/resources";
 
+// Whenever a list of resource is shown, use this order
 export const resourceOrder = [
     Resource.food,
     Resource.wood,
+    Resource.iron,
     Resource.stone,
     Resource.fabric,
     Resource.leather,
