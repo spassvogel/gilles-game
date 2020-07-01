d8b55306f2130273db0db946e80d6d72557f2bb1
Wouter van den Heuvel <exo_00@hotmail.com>
2020-07-01 21:19:11 +0200

    Updates

---
+++ precache-manifest.821f974f2c3118179393f2926dd1dba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75294471dc0591a641f9eeac2fab0e12",
+    "url": "./gilles-game/index.html"
+  },
+  {
+    "revision": "a159271e0a54dd6e2e2d",
+    "url": "./gilles-game/static/css/main.cd6f555f.chunk.css"
+  },
+  {
+    "revision": "319a21ff48e80cb83821",
+    "url": "./gilles-game/static/js/2.f934410a.chunk.js"
+  },
+  {
+    "revision": "a33b01e770eb485fcdbc8c50acb8de31",
+    "url": "./gilles-game/static/js/2.f934410a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a159271e0a54dd6e2e2d",
+    "url": "./gilles-game/static/js/main.cf3b3d46.chunk.js"
+  },
+  {
+    "revision": "09804a3ec4e14bdec8bd",
+    "url": "./gilles-game/static/js/runtime-main.2d0f9708.js"
+  }
+]);
