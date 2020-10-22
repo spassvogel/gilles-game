7b7b5e92271799301ec0b8217a3d8a93d64489f4
Wouter van den Heuvel <exo_00@hotmail.com>
2020-10-22 23:23:04 +0200

    fix: an issue where scene would be stuck in 'loading'

--- src/components/world/QuestPanel/context/SceneControllerContext.tsx
+++ src/components/world/QuestPanel/context/SceneControllerContext.tsx
@@ -41,7 +41,7 @@ const SceneControllerContextProvider = (props: PropsWithChildren<Props>) => {
         }
     }, [controller, questName, scene, sceneName]);
 
-    if (controller && !(controller.dataLoaded || loaded)) {
+    if (controller && (!controller.dataLoaded || !loaded)) {
         return (
             <div>loading...</div>
         );
