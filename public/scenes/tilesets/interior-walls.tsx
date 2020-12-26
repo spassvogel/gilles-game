726222d3cae7e9958572527aeabffae39aeefcec
Wouter van den Heuvel <exo_00@hotmail.com>
2020-12-26 11:28:35 +0100

    feat: added door to interior wall tileset

---
+++ public/scenes/tilesets/interior-walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="interior-walls" tilewidth="100" tileheight="100" tilecount="60" columns="10">
+ <image source="interior-walls.png" width="1000" height="600"/>
+</tileset>
