67494b1e087abce15f9d985a4ee69f844580408d
Wouter van den Heuvel <exo_00@hotmail.com>
2021-01-31 01:55:52 +0100

    feat: styling of Select

---
+++ src/components/ui/common/Select/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Select, { Props} from 'react-select';
+
+
+export default function (props: Props) {
+    return <Select
+        styles={{
+            control: (provided, state) => ({
+                ...provided,
+                minHeight: 'inherit',
+                height: '32px',
+                boxShadow: '0',
+                border: state.isFocused ? '1px solid orange' : '1px solid #B1B1B1',
+                ':hover': {
+                    border: '1px solid orange'
+                }             
+            }),
+            container: (provided, state) => ({
+                ...provided,
+                flex: 1
+            }),
+            option: (provided) => ({
+                ...provided,
+                padding: '4px 12px',
+            }),
+            placeholder: (provided) => ({
+                ...provided,
+                whiteSpace: 'nowrap'
+            }), 
+            valueContainer: (provided) => ({
+                ...provided,
+                marginTop: '-2px'
+            }), 
+            indicatorsContainer: (provided) => ({
+                ...provided,
+                marginTop: '-2px',
+
+                'div': {
+                    padding: '4px'
+                }
+            }),
+            
+        }}
+        {...props}
+    />
+}
