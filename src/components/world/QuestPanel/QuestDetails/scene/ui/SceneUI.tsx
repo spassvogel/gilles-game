5ed60241f599e6ac6c8faaaa8a0f5f32a8f27e81
Wouter van den Heuvel <exo_00@hotmail.com>
2021-02-17 19:01:02 +0100

    removed html actorthingy

--- src/components/world/QuestPanel/QuestDetails/scene/ui/SceneUI.tsx
+++ src/components/world/QuestPanel/QuestDetails/scene/ui/SceneUI.tsx
@@ -3,7 +3,6 @@ import { useQuest } from 'hooks/store/quests';
 import React, { PropsWithChildren, useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { SceneActionType } from 'store/types/scene';
 import { locationEquals } from 'utils/tilemap';
-import ActorThingy from './ActorThingy';
 import CombatUIWidget from './CombatUIWidget';
 import NormalUICursor from './NormalUICursor';
 import "./styles/sceneUI.scss";
@@ -177,9 +176,6 @@ const SceneUI = (props: PropsWithChildren<Props>) => {
             onMouseUp={handleMouseUp}
         >
             {children}
-            {scene.combat && controller.sceneActors.map(a => (
-                <ActorThingy key={a.id} actor={a} />
-            ))}
             {!scene.combat && cursorLocation && (
                 <NormalUICursor location={cursorLocation} />
             )}
