c93cccf09df2e9a046bc35cbe42de4f126de7d31
Wouter van den Heuvel <exo_00@hotmail.com>
2021-07-08 12:43:55 +0200

    Updates

---
+++ scenes/tilesets/interior-walls-64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="interior-walls-64" tilewidth="64" tileheight="64" tilecount="144" columns="12">
+ <image source="interior-walls-64.png" width="768" height="768"/>
+</tileset>
