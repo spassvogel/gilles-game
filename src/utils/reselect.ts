6fca92601c582d87200286c76af8344b9cc43695
Wouter van den Heuvel <exo_00@hotmail.com>
2021-10-18 09:43:19 +0200

    updated reselect and fixed typing issue

--- src/utils/reselect.ts
+++ src/utils/reselect.ts
@@ -1,9 +1,7 @@
 import { createSelectorCreator, defaultMemoize } from 'reselect';
 import stringArrayEqual from 'string-arrays-equal';
 
-export const createStringArraySelector = createSelectorCreator<string>(
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore: https://github.com/reduxjs/reselect/issues/384
+export const createStringArraySelector = createSelectorCreator(
   defaultMemoize,
   stringArrayEqual
 );
