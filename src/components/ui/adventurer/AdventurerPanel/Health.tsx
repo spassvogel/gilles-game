c41d6f1467498e3994787f49c1adcc01aaec5bb1
Wouter van den Heuvel <exo_00@hotmail.com>
2021-12-08 17:11:29 +0100

    healthbar functional

--- src/components/ui/adventurer/AdventurerPanel/Health.tsx
+++ src/components/ui/adventurer/AdventurerPanel/Health.tsx
@@ -2,24 +2,28 @@ import * as React from "react";
 import PlainProgressbar from 'components/ui/common/progress/PlainProgressbar';
 import { useAdventurerState } from "hooks/store/adventurers";
 import { TextManager } from "global/TextManager";
+import { xpToLevel } from "mechanics/adventurers/levels";
+import { calculateBaseHitpoints } from "mechanics/adventurers/hitpoints";
 
 export interface Props {
-    adventurerId: string;
+  adventurerId: string;
 }
 
 const Health = (props: Props) => {
-    const { adventurerId } = props;
-    const { health } = useAdventurerState(adventurerId);
+  const { adventurerId } = props;
+  const { health, basicAttributes, xp } = useAdventurerState(adventurerId);
+  const level = xpToLevel(xp);
+  const baseHP = calculateBaseHitpoints(level, basicAttributes.for);
 
-    return (
-        <div className="health">
-            {TextManager.get("ui-adventurer-info-health")}
-            <PlainProgressbar
-                progress={health / 100}
-                label={`${health.toFixed(2)}%`}
-                variation="health"
-            />
-        </div>
-    )
+  return (
+    <div className="health">
+      {TextManager.get("ui-adventurer-info-health")}
+      <PlainProgressbar
+        progress={health / baseHP}
+        label={`${health.toFixed(2)}/${baseHP}`}
+        variation="health"
+      />
+    </div>
+  )
 }
 export default Health;
