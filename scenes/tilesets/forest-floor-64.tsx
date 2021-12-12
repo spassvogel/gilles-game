b8b27ae46619c60a258c2e5fcdea3cf84ac96706
Wouter van den Heuvel <exo_00@hotmail.com>
2021-12-12 11:15:01 +0100

    Updates

--- scenes/tilesets/forest-floor-64.tsx
+++ scenes/tilesets/forest-floor-64.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="forest-floor-64" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+<tileset version="1.5" tiledversion="1.7.1" name="forest-floor-64" tilewidth="64" tileheight="64" tilecount="256" columns="16">
  <image source="forest-floor-64.png" width="1024" height="1024"/>
  <wangsets>
   <wangset name="Unnamed Set" type="corner" tile="-1">
    <wangcolor name="grass" color="#2ded16" tile="-1" probability="1"/>
    <wangcolor name="rubble" color="#f5c51d" tile="-1" probability="1"/>
-   <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="cobblestones" color="#0000ff" tile="-1" probability="1"/>
    <wangcolor name="stone" color="#ff7700" tile="-1" probability="1"/>
    <wangtile tileid="7" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="18" wangid="0,1,0,1,0,1,0,1"/>
@@ -26,24 +26,46 @@
    <wangtile tileid="121" wangid="0,2,0,4,0,2,0,2"/>
    <wangtile tileid="122" wangid="0,2,0,4,0,4,0,2"/>
    <wangtile tileid="123" wangid="0,2,0,2,0,4,0,2"/>
+   <wangtile tileid="124" wangid="0,1,0,4,0,1,0,1"/>
+   <wangtile tileid="125" wangid="0,1,0,4,0,4,0,1"/>
+   <wangtile tileid="126" wangid="0,1,0,1,0,4,0,1"/>
    <wangtile tileid="137" wangid="0,4,0,4,0,2,0,2"/>
    <wangtile tileid="138" wangid="0,4,0,4,0,4,0,4"/>
    <wangtile tileid="139" wangid="0,2,0,2,0,4,0,4"/>
+   <wangtile tileid="140" wangid="0,4,0,4,0,1,0,1"/>
+   <wangtile tileid="141" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="142" wangid="0,1,0,1,0,4,0,4"/>
    <wangtile tileid="153" wangid="0,4,0,4,0,2,0,2"/>
    <wangtile tileid="154" wangid="0,4,0,4,0,4,0,4"/>
    <wangtile tileid="155" wangid="0,2,0,2,0,4,0,4"/>
+   <wangtile tileid="156" wangid="0,4,0,1,0,1,0,1"/>
+   <wangtile tileid="157" wangid="0,4,0,1,0,1,0,4"/>
+   <wangtile tileid="158" wangid="0,1,0,1,0,1,0,4"/>
    <wangtile tileid="169" wangid="0,4,0,2,0,2,0,2"/>
    <wangtile tileid="170" wangid="0,4,0,2,0,2,0,4"/>
    <wangtile tileid="171" wangid="0,2,0,2,0,2,0,4"/>
+   <wangtile tileid="172" wangid="0,4,0,1,0,4,0,4"/>
+   <wangtile tileid="173" wangid="0,4,0,4,0,1,0,4"/>
+   <wangtile tileid="188" wangid="0,1,0,4,0,4,0,4"/>
+   <wangtile tileid="189" wangid="0,4,0,4,0,4,0,1"/>
    <wangtile tileid="192" wangid="0,0,0,3,0,0,0,0"/>
    <wangtile tileid="193" wangid="0,0,0,3,0,3,0,0"/>
    <wangtile tileid="194" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="201" wangid="0,2,0,3,0,2,0,2"/>
+   <wangtile tileid="202" wangid="0,2,0,3,0,3,0,2"/>
+   <wangtile tileid="203" wangid="0,2,0,2,0,3,0,2"/>
    <wangtile tileid="208" wangid="0,3,0,3,0,0,0,0"/>
    <wangtile tileid="209" wangid="0,3,0,3,0,3,0,3"/>
    <wangtile tileid="210" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="217" wangid="0,3,0,3,0,2,0,2"/>
+   <wangtile tileid="218" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="219" wangid="0,2,0,2,0,3,0,3"/>
    <wangtile tileid="224" wangid="0,3,0,0,0,0,0,0"/>
    <wangtile tileid="225" wangid="0,3,0,0,0,0,0,3"/>
    <wangtile tileid="226" wangid="0,0,0,0,0,0,0,3"/>
+   <wangtile tileid="233" wangid="0,3,0,2,0,2,0,2"/>
+   <wangtile tileid="234" wangid="0,3,0,2,0,2,0,3"/>
+   <wangtile tileid="235" wangid="0,2,0,2,0,2,0,3"/>
   </wangset>
  </wangsets>
 </tileset>
