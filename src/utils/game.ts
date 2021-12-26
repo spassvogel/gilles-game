a166a7b84d9aae383c02660363ccf060b10ab356
Wouter van den Heuvel <exo_00@hotmail.com>
2021-12-26 10:16:55 +0100

    prepare for webpack 5

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -4,16 +4,16 @@ const key = "P5mw}jD>5c6Y]yqy";
 const encryptor = createEncryptor(key);
 
 export const saveGame = (storeState: StoreState) => {
-    const a = document.createElement('a');
-    const text = JSON.stringify(storeState);
-    const encrypted = encryptor.encrypt(text);
-    const filename = "Gidletown save.json";
-    a.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(encrypted));
-    a.setAttribute('download', filename);
-    a.click();
+  const a = document.createElement('a');
+  const text = JSON.stringify(storeState);
+  const encrypted = encryptor.encrypt(text);
+  const filename = "Gidletown save.json";
+  a.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(encrypted));
+  a.setAttribute('download', filename);
+  a.click();
 }
 
 export const decryptSavedGame = (encrypted: string) : StoreState => {
-    const text = encryptor.decrypt(encrypted);
-    return JSON.parse(text);
+  const text = encryptor.decrypt(encrypted);
+  return JSON.parse(text);
 }
