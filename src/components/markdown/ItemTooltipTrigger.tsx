862607bf06f65d49d27018758aa3c45c75dff225
Wouter van den Heuvel <exo_00@hotmail.com>
2022-01-12 00:09:56 +0100

    colored triggers

--- src/components/markdown/ItemTooltipTrigger.tsx
+++ src/components/markdown/ItemTooltipTrigger.tsx
@@ -3,11 +3,30 @@ import { ContextType } from 'constants/context';
 import { TextManager } from 'global/TextManager';
 import { TooltipManager } from 'global/TooltipManager';
 import { ElementContent } from 'react-markdown/lib/ast-to-react';
+import { getDefinition } from 'definitions/items';
+import { Rarity } from 'constants/items';
 
 type Props = {
   children: ReactNode[]
 };
 
+const getRarityClassName = (rarity?: Rarity): string => {
+  switch (rarity) {
+    case Rarity.common:
+      return 'item-trigger-common';
+    case Rarity.uncommon:
+      return 'item-trigger-uncommon';
+    case Rarity.rare:
+      return 'item-trigger-rare';
+    case Rarity.epic:
+      return 'item-trigger-epic';
+    case Rarity.legendary:
+      return 'item-trigger-legendary';
+  }
+  return '';
+};
+
+// To be used inside Markdown component
 const ItemTooltipTrigger = (props: Props) => {
   const { children } = props;
 
@@ -16,6 +35,11 @@ const ItemTooltipTrigger = (props: Props) => {
       return JSON.parse(children[0]);
     }
   }, [children]);
+
+  const definition = useMemo(() => {
+    if (!item) return;
+    return getDefinition(item.type);
+  }, [item]);
 
   const handleClick = (event: React.MouseEvent) => {
     const origin = (event.currentTarget as HTMLElement);
@@ -28,7 +52,7 @@ const ItemTooltipTrigger = (props: Props) => {
   return (
     <>
       [
-      <span className="trigger" onClick={handleClick}>
+      <span className={`trigger ${getRarityClassName(definition?.rarity)}`} onClick={handleClick}>
         {TextManager.getItemName(item)}
       </span>
       ]
