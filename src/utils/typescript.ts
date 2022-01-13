174de84514c05ba27efb9c0a7abf2354338084da
Wouter van den Heuvel <exo_00@hotmail.com>
2022-01-14 00:23:22 +0100

    added listEnum

--- src/utils/typescript.ts
+++ src/utils/typescript.ts
@@ -7,9 +7,17 @@ export type Entries<T> = {
   [K in keyof T]: [K, T[K]]
 }[keyof T][];
 
-export type DistributiveOmit<T, K extends keyof any> = T extends unknown ? Omit<T, K>: never;
 
+// Type save entries ([key, value] pair)
 export function entries<T>(obj: T): Entries<T> {
   return Object.entries(obj) as Entries<T>;
 }
+
+export type DistributiveOmit<T, K extends keyof any> = T extends unknown ? Omit<T, K>: never;
+
+
+// Returns a list of enum values
+export const listEnum = <T extends { [key: string]: number | string }>(enumeration: T) => {
+  return Object.keys(enumeration).filter(v => !isNaN(Number(v))).map(v => Number(v));
+};
 
