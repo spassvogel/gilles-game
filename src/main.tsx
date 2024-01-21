e2fd3bd5cc1c7492683c873be4b65cf588f2b36c
Wouter van den Heuvel <exo_00@hotmail.com>
2024-01-21 02:04:31 +0100

    initialize random

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { Assets } from 'pixi.js'
 import { createRoot } from 'react-dom/client'
 import { TextManager } from 'global/TextManager'
 import Game from 'components/Game'
+import * as random from './utils/random'
 import './index.css'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 const texts = await Assets.load<Record<string, string>>('/lang/en.json')
 TextManager.init(texts)
+
+random.init('GILLESROX2')
 
 root.render(
   // <React.StrictMode>
