a60645e9572c6136e374d47b641f3894f4842d1c
Wouter van den Heuvel <exo_00@hotmail.com>
2024-03-25 21:00:02 -0300

    fixed order

--- src/components/world/QuestPanel/CombatBar/CombatBar.tsx
+++ src/components/world/QuestPanel/CombatBar/CombatBar.tsx
@@ -4,6 +4,7 @@ import * as TextManager from 'global/TextManager'
 import Button from 'components/ui/buttons/Button'
 import { useQuest } from 'hooks/store/quests'
 import { Allegiance } from 'store/types/scene'
+
 import './styles/combatBar.scss'
 
 type Props = {
@@ -16,12 +17,16 @@ const CombatBar = (props: Props) => {
   const controller = useContext(SceneControllerContext)
   const quest = useQuest(questName)
 
+  const endTurnDisabled = quest.scene?.turn !== Allegiance.player || (quest.scene?.actionQueue?.length ?? 0) > 0
+
   return (
     <div className="combat-bar">
       <div className="title">
         {TextManager.get(`ui-combat-bar-${Allegiance[quest.scene?.turn ?? Allegiance.player]}-turn`)}
       </div>
-      <Button size="medium" onClick={() => controller?.endTurn()} disabled={quest.scene?.turn !== Allegiance.player}>End turn</Button>
+      <Button size="medium" onClick={() => controller?.endTurn()} disabled={endTurnDisabled}>
+        {TextManager.get('ui-combat-bar-end-turn')}
+      </Button>
     </div>
   )
 }
