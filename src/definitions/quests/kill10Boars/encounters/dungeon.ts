e2f4662fc499174e692add19750608df9394a87d
Wouter van den Heuvel <exo_00@hotmail.com>
2024-05-07 21:26:52 -0300

    fixed a bug where adventurer name would not show in chest opening log

--- src/definitions/quests/kill10Boars/encounters/dungeon.ts
+++ src/definitions/quests/kill10Boars/encounters/dungeon.ts
@@ -89,7 +89,7 @@ export class DungeonEntranceSceneController extends DungeonEncounterSceneControl
       // todo: I want to share this common stuff with other SceneControllers
       case 'chest':
         if (!this.questVars.dungeon.entrance.chestOpen) {
-          const textEntry = { key: 'quest-common-adventurer-opened-chest', context: { adventurer: getUniqueName(actor) } }
+          const textEntry = { key: 'quest-common-adventurer-opened-chest', context: { adventurer: actor } }
           this.questUpdate(textEntry, '/img/items/misc/chest-02.png')
 
           this.updateQuestVars({
